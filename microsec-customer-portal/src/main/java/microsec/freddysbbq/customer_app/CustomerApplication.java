9ba1ffad8eab355799b3c6e1013b0db581ba7f09
Will Tran <wtran@pivotal.io>
2016-07-18 18:13:10 -0400

    Setup our own @LoadBalanced OAuth2RestTemplate [1]
    [1]
    https://spring.io/blog/2016/05/11/spring-cloud-brixton-release-is-available#notes

--- microsec-customer-portal/src/main/java/microsec/freddysbbq/customer_app/CustomerApplication.java
+++ microsec-customer-portal/src/main/java/microsec/freddysbbq/customer_app/CustomerApplication.java
@@ -2,16 +2,14 @@ package microsec.freddysbbq.customer_app;
 
 import java.util.Arrays;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.hal.Jackson2HalModule;
@@ -19,7 +17,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -51,13 +51,6 @@ public class CustomerApplication extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
     }
 
-    @Autowired
-    @Qualifier("loadBalancedOauth2RestTemplate")
-    private OAuth2RestTemplate oauth2RestTemplate;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Bean
     public Targets targets() {
         return new Targets();
@@ -73,14 +66,20 @@ public class CustomerApplication extends WebSecurityConfigurerAdapter {
         return new MenuBootstrap();
     }
 
-    @PostConstruct
-    public void halConfig() {
+    @LoadBalanced
+    @Bean
+    public OAuth2RestTemplate loadBalancedOauth2RestTemplate(
+            OAuth2ProtectedResourceDetails resource,
+            OAuth2ClientContext oauth2Context,
+            ObjectMapper objectMapper) {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.registerModule(new Jackson2HalModule());
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setSupportedMediaTypes(MediaType.parseMediaTypes("application/hal+json"));
         converter.setObjectMapper(objectMapper);
+        OAuth2RestTemplate oauth2RestTemplate = new OAuth2RestTemplate(resource, oauth2Context);
         oauth2RestTemplate.setMessageConverters(Arrays.asList(converter));
+        return oauth2RestTemplate;
     }
 
 }
