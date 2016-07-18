92b9234c845b109fc5f856f314d0c36081feacfa
Will Tran <wtran@pivotal.io>
2016-07-18 17:43:10 -0400

    Define our own token relay behaviour.
    Autowired token relay behaviour now requires an actual client registered
    with UAA because we pre-emptively authenticate[1] and there is a
    non-null default clientSecret[2]. For clarity, just do it ourselves.
    
    [1]https://github.com/spring-projects/spring-boot/blob/v1.3.6.RELEASE/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/resource/ResourceServerTokenServicesConfiguration.java#L289-L292
    [2]https://github.com/spring-projects/spring-boot/blob/v1.3.6.RELEASE/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/security/oauth2/OAuth2ClientProperties.java#L41

--- microsec-order-service/src/main/java/microsec/freddysbbq/order/OrderApplication.java
+++ microsec-order-service/src/main/java/microsec/freddysbbq/order/OrderApplication.java
@@ -5,24 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 import microsec.common.Targets;
+import microsec.common.TokenRelayingRestTemplate;
 import microsec.freddysbbq.order.model.v1.Order;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = Order.class)
 @EnableResourceServer
-@EnableOAuth2Client
 @EnableDiscoveryClient
 public class OrderApplication {
 
@@ -60,6 +61,12 @@ public class OrderApplication {
                         .antMatchers("/orders/**").access("#oauth2.hasScope('order.admin')");
             }
         };
+    }
+
+    @LoadBalanced
+    @Bean
+    public OAuth2RestTemplate loadBalancedTokenRelayingRestTemplate() {
+        return new TokenRelayingRestTemplate();
     }
 
 }
